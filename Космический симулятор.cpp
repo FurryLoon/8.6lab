c15dc8befd2250c9b4d09d71ead5148dab238974
Maksim <maksim.antonov2012@yandex.ru>
2024-08-02 08:48:41 +0300

    1

---
+++ Космический симулятор.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()	{
+	setlocale(LC_ALL, "Russian");
+	float F,m,t,a,b;
+	std::cout << "Введите массу, силу тяги и время звездолёта: ";
+	std::cin >> m >> F >> t;
+	a = F / m;
+	b = (a * std::pow(t, 2)) / 2;
+	
+	std::cout << "Звездолёт окажется в: " << b << " еденицах от изначальной точки";
+
+}
